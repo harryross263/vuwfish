c1e8eb0a6f544d51f0d6fca6ebf3e754ab16beaa
Harry Ross <harryross263@gmail.com>
2016-06-09 16:18:49 +1200

    Fixed index.js app listening error

--- index.js
+++ index.js
@@ -9,3 +9,6 @@ app.get('/', function(request, response) {
   response.render('index.html');
 });
 
+app.listen(app.get('port'), function() {
+  console.log('Node app is running on port', app.get('port'));
+m});
