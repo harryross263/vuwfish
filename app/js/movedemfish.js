f98924ff75e4ead50c4b46ff587631717b7328bd
Harry Ross <harryross263@gmail.com>
2016-06-10 20:59:07 +1200

    Testing animation toggle

--- app/js/movedemfish.js
+++ app/js/movedemfish.js
@@ -11,5 +11,13 @@ $(document).ready(function() {
 		$(".fish").fadeToggle(1000);
 	});
 
+	$(window).keypress(function (e) {
+		if (e.which == 0 || e.which == 32) {
+			e.preventDefault();
+			$("#canvas-wrapper").fadeToggle(1000);
+			$(".fish").fadeToggle(1000);
+		}
+	});
+
 	justKeepSwimming();
 });
