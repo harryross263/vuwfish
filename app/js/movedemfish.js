056bcf10198738ad14d27c266b9a0085aaad78fe
Harry Ross <harryross263@gmail.com>
2016-06-28 13:46:46 +1200

    Refactoring.

--- app/js/movedemfish.js
+++ app/js/movedemfish.js
@@ -1,12 +1,19 @@
 $(document).ready(function() {
+	$(window).focus();
+	
 	function justKeepSwimming() {
 		$(".fish").each(function() {
-			$(this).css('right', Math.random()*-1000);
-			$(this).animate({right: $(document).width()}, Math.random()*500 + 4000, "linear", justKeepSwimming).delay(Math.random()*1000);
+			$(this).css('right', Math.random() * -1000);
+			$(this).animate(
+				{
+					right: $(document).width()
+				},
+				Math.random() * 500 + 4000,
+				"linear",
+				justKeepSwimming)
+			.delay(Math.random() * 1000);
 		});
 	}
-
-	$(window).focus();
 
 	$(window).keypress(function (e) {
 		if (e.which == 0 || e.which == 32) {
